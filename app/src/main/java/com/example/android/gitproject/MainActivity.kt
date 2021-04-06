f0f503f7148d3e8abd964cab50101b6470d07ffb
Raj <rajgadhiya011@gmail.com>
2021-04-06 17:59:33 +0530

    third commit

--- app/src/main/java/com/example/android/gitproject/MainActivity.kt
+++ app/src/main/java/com/example/android/gitproject/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("this will included in my second commit")
+        println("this will included in my third commit")
     }
 }
